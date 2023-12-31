e59e5d05e6639d9379e670c3ff0c272ec92d22f5
Josh <joshuarclarke88@gmail.com>
2023-12-31 21:22:43 +0100

    initial commit

---
+++ src/main/java/com/powerjack/ocpi/OcpiApplication.java
@@ -0,0 +1,13 @@
+package com.powerjack.ocpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powerjack/ocpi/OcpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerjack.ocpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
